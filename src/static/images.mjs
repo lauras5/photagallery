3054c1123ca2ae08603a924592dcd66bff7bfd62
Lauras5 <idlaurasalcido@gmail.com>
2020-12-27 18:50:58 -0800

    async/await fix, remove unused code, remove unused classes

--- src/static/images.mjs
+++ src/static/images.mjs
@@ -1,50 +1,48 @@
-export async function loadImages() {
-    // loading bar
-    const loader = document.getElementById('loader');
-    loader.classList.add('loading--loading');
+function imageComponent(img) {
+    const imgCard = document.createElement('div');
+    imgCard.classList.add('images__image-card');
+    imgCard.classList.add('images__image-card--hide');
 
+    const id = img.id;
+    const imgDiv = document.createElement('img');
+    const imgURL = `http://localhost:3000/api/image?id=${id}&res=low`
+
+    imgDiv.src = imgURL;
+    imgDiv.classList.add('images__image');
+    imgDiv.onload = function() {
+        const loader = document.getElementById('loader');
+        loader.classList.add('loading--complete');
+        imgCard.classList.remove('images__image-card--hide')
+    }
+
+    imgCard.appendChild(imgDiv);
+
+    return imgCard;
+}
+
+async function fetchImages() {
+    const response = await fetch('http://localhost:3000/api/image-metadata');
+    if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+    } else {
+        const images = await response.json();
+        const imageContainer = document.getElementById('images');
+        images.map(e => imageContainer.append(imageComponent(e)));
+    }
+}
+
+export function loadImages() {
+    const loader = document.getElementById('loader');
     const loadText = document.createElement('span');
     loadText.innerText = 'Loading...';
     loadText.classList.add('loading__text');
 
     loader.appendChild(loadText);
 
-    await fetch('http://localhost:3000/api/image-metadata')
-        .then(response => response.json())
-        .then(data => {
-            // Add images to container
-            const imageContainer = document.getElementById('images');
-            imageContainer.classList.add('images--hide');
-
-            data.map(img => {
-                const imgCard = document.createElement('div');
-                imgCard.classList.add('images__image-card');
-
-                const id = img.id;
-                const imgDiv = document.createElement('img');
-                const imgURL = `http://localhost:3000/api/image?id=${id}&res=low`
-
-                imgDiv.src = imgURL;
-                imgDiv.dataset.id = img.id;
-                imgDiv.classList.add('images__image');
-                imgDiv.onload = function() {
-                    loader.classList.remove('loading--loading');
-                    loader.classList.add('loading--complete');
-                    imageContainer.classList.remove('images--hide');
-                }
-
-                imgCard.appendChild(imgDiv);
-                imageContainer.append(imgCard);
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            loader.classList.remove('loading--loading');
-            loader.classList.add('loading--error');
-        })
-
-
+    fetchImages().catch(e => { console.log(e) });
 }
+
 
 
 
